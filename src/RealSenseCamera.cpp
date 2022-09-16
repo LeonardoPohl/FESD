297a3db2fe76d6921600f1d0dc62be79332b27c6
LeonardoPohl <pohly3@gmail.com>
2022-09-16 15:50:06 +0200

    Scaled realsense stream

--- src/RealSenseCamera.cpp
+++ src/RealSenseCamera.cpp
@@ -48,14 +48,14 @@ RealSenseCamera::~RealSenseCamera() {
 
 cv::Mat RealSenseCamera::getFrame() {
 	frameset data = this->_pipe.wait_for_frames(); // Wait for next set of frames from the camera
-	frame depth = data.get_depth_frame();//.apply_filter(this->_color_map);
+	frame depth = data.get_depth_frame();
 
 	// Query frame size (width and height)
 	const int w = depth.as<video_frame>().get_width();
 	const int h = depth.as<video_frame>().get_height();
 
 	// Create OpenCV matrix of size (w,h) from the colorized depth data
-	return cv::Mat(cv::Size(w, h), CV_16UC1, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
+	return cv::Mat(cv::Size(w, h), CV_16UC1, (void*)depth.get_data(), cv::Mat::AUTO_STEP) * 10;
 }
 
 // Utils
