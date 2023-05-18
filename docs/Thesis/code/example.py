efa1cd32ab1ea6996c839889f6782782233b14dc
LeonardoPohl <pohly3@gmail.com>
2023-05-18 13:20:36 +1000

    Thesis Restructure

--- docs/Thesis/code/example.py
+++
@@ -1,9 +0,0 @@
-from distributed_execution import DistributedExecution
-
-def square(v: int) -> int:
-    return v * v
-
-values = list(range(1, 10001))
-
-with DistributedExecution() as d:
-    results = d.map(square, values, chunk_size=100)
